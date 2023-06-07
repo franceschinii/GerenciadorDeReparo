ddc7ff749d4b41f814cf3203179866010b814229
Poodle Moth <110733000+WhitePoodleMoth@users.noreply.github.com>
2023-06-06 17:17:56 -0700

    Project Structure

--- ToyotaManager/src/toyotamanager/ToyotaManager.java
+++ ToyotaManager/src/toyotamanager/ToyotaManager.java
@@ -4,6 +4,8 @@
  */
 package toyotamanager;
 
+import views.splash;
+
 /**
  *
  * @author UnknownAccount
@@ -14,7 +16,8 @@ public class ToyotaManager {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        splash page = new splash();
+        page.setVisible(true);
     }
     
 }
